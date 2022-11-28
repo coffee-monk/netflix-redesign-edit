e9c8dd89d7037dffac836ea8eaa8f02bae16338a
coffee-monk <coffeemonk@hotmail.com>
2022-11-28 14:34:40 -0500

    main front end UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header from '../components/Header'
 import Banner from '../components/Banner'
+import Header from '../components/Header'
+import Row from '../components/Row'
 import requests from '../utils/requests'
+import { Movie } from '../typings'
+import { accessSync } from 'fs'
 
-const Home: NextPage = () => {
+interface Props {
+    netflixOriginals: Movie[]
+    trendingNow: Movie[]
+    topRated: Movie[]
+    actionMovies: Movie[]
+    comedyMovies: Movie[]
+    horrorMovies: Movie[]
+    romanceMovies: Movie[]
+    documentaries: Movie[]
+}
+
+const Home = ({
+    netflixOriginals,
+    actionMovies,
+    comedyMovies,
+    documentaries,
+    horrorMovies,
+    romanceMovies,
+    topRated,
+    trendingNow,
+}: Props) => {
     return (
         <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
             <Head>
@@ -12,8 +34,17 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-            <main>
-                <Banner />
+            <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+                <Banner netflixOriginals={netflixOriginals} />
+                <section className='md:space-y-24'>
+                    <Row title="Trending Now" movies={trendingNow} />
+                    <Row title="Top Rated" movies={topRated} />
+                    <Row title="Action Thrillers" movies={actionMovies} />
+                    <Row title="Comedies" movies={comedyMovies} />
+                    <Row title="Scary Movies" movies={horrorMovies} />
+                    <Row title="Romance Movies" movies={romanceMovies} />
+                    <Row title="Documentaries" movies={documentaries} />
+                </section>
             </main>
         </div>
     )
@@ -41,4 +72,17 @@ export const getServerSideProps = async () => {
         fetch(requests.fetchRomanceMovies).then((res) => res.json()),
         fetch(requests.fetchDocumentaries).then((res) => res.json()),
     ])
+
+    return {
+        props: {
+            netflixOriginals: netflixOriginals.results,
+            trendingNow: trendingNow.results,
+            topRated: topRated.results,
+            actionMovies: actionMovies.results,
+            comedyMovies: comedyMovies.results,
+            horrorMovies: horrorMovies.results,
+            romanceMovies: romanceMovies.results,
+            documentaries: documentaries.results,
+        },
+    }
 }
